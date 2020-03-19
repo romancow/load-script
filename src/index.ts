dee9acba7e3e1f4c86064f2c6eece28082a47f29
Sean Wilson <seancwilson@gmail.com>
2020-03-19 12:01:20 -0500

    update type definitions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
-import LoadScriptPromise, { LoadScriptOptions } from './load-script-promise'
+import LSPromise, { LoadScriptPromiseOptions } from './promise'
 
-export default async function loadScript<T>(path: string, options?: LoadScriptOptions) {
-	const promise = LoadScriptPromise.get(path, options)
-	return promise.map(options?.map)
+export type LoadScriptOptions<T> = LoadScriptPromiseOptions & {
+	map?: T extends keyof Window ? T : (event: Event) => T
 }
+
+function loadScript(path: string, options?: LoadScriptPromiseOptions): Promise<HTMLScriptElement>
+function loadScript<K extends keyof Window>(path: string, options: LoadScriptOptions<K>): Promise<Window[K]>
+function loadScript<T>(path: string, options: LoadScriptOptions<T>): Promise<T>
+function loadScript<T>(path: string, options?: LoadScriptOptions<T>) {
+	const promise = LSPromise.get(path, options)
+	return promise.map<T>(options?.map)
+}
+
+export default loadScript
--- src/promise.ts
+++ src/promise.ts
@@ -1,31 +1,19 @@
 import * as Utilities from './utilities'
 
-type LoadScriptMapFn = <T>(this: LoadScriptPromise, event: Event) => T
-
-namespace LoadScriptMapFn {
-	export function ensure(fn?: string | LoadScriptMapFn): LoadScriptMapFn | null {
-		return (fn != null) ? (fn instanceof Function) ?
-			fn : () => (window as any)[fn] : null
-	}
-}
-
-type LoadScriptOptions = {
+export type LoadScriptPromiseOptions = {
 	type?: string
 	async?: boolean
 	id?: string
 	parent?: string | Element
-	map?: string | LoadScriptMapFn,
 	cache?: boolean,
 	force?: boolean
 }
-
-export { LoadScriptOptions, LoadScriptMapFn }
 
 export default class LoadScriptPromise extends Promise<Event> {
 
 	element!: HTMLScriptElement
 
-	constructor(readonly path: string, readonly options: LoadScriptOptions = {}) {
+	constructor(readonly path: string, readonly options: LoadScriptPromiseOptions = {}) {
 		super((onload, onerror) => {
 			const attrs = { ...this.attrs, onload, onerror }
 			this.element = Utilities.Element.create('script', attrs)
@@ -45,15 +33,16 @@ export default class LoadScriptPromise extends Promise<Event> {
 		return Utilities.Object.compact({ src, type, async, id})
 	}
 
-	map(map: string | LoadScriptMapFn = (() => this.element) as LoadScriptMapFn) {
-		const mapFn = LoadScriptMapFn.ensure(map)
-		return this.then(mapFn)
+	map<T = HTMLScriptElement>(map?: T extends keyof Window ? T : ((ev: Event) => T)) {
+		const mapFn = (map != null) ? (map instanceof Function) ?
+			map : () => window[map as keyof Window] : () => this.element
+		return this.then(mapFn as (ev: Event) => T)
 	}
 
 
 	static Cache = {} as { [id: string]: LoadScriptPromise }
 
-	static get(path: string, options: LoadScriptOptions = {}) {
+	static get(path: string, options: LoadScriptPromiseOptions = {}) {
 		const { id = path, cache = false } = options
 		const lsCache = cache ? this.Cache : {}
 		return lsCache[id] ?? (lsCache[id] = new this(path, options))
