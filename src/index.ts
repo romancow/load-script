dd854c3afdae24cb520c9f61bc3d0dc81bf35ee2
Sean Wilson <seancwilson@gmail.com>
2020-06-24 16:24:09 -0500

    update type definitions

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,9 @@ import LSPromise, { LoadScriptPromiseOptions } from './promise'
  * Script load options
  * More details on the attribute options [here](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/script#Attributes).
  * */
-export type LoadScriptOptions<T> = LoadScriptPromiseOptions & {
+export type LoadScriptOptions = LoadScriptPromiseOptions & {
 	/** Whether to get/set the script in cache */
 	cache?: boolean
-	/** The key of the window value or retrieval function for the `loadScript` function to return */
-	map?: T extends keyof Window ? T : (event: Event) => T
 }
 
 /**
@@ -18,7 +16,7 @@ export type LoadScriptOptions<T> = LoadScriptPromiseOptions & {
  * @param options - Script load options
  * @returns A promise for the loaded script's `HTMLScriptElement`
  */
-function loadScript(url: string, options?: LoadScriptPromiseOptions): Promise<HTMLScriptElement>
+function loadScript(url: string, options?: LoadScriptOptions): Promise<HTMLScriptElement>
 /**
  * Loads the given script as an `HTMLScriptElement`.
  *
@@ -27,7 +25,7 @@ function loadScript(url: string, options?: LoadScriptPromiseOptions): Promise<HT
  * @param options - Script load options with a `map` key of a window value to return
  * @returns A promise for the specified Window value after the script is loaded
  */
-function loadScript<K extends keyof Window>(url: string, options: LoadScriptOptions<K>): Promise<Window[K]>
+function loadScript<K extends keyof Window>(url: string, options: LoadScriptOptions & { map: K }): Promise<Window[K]>
 /**
  * Loads the given script as an `HTMLScriptElement`.
  *
@@ -36,11 +34,11 @@ function loadScript<K extends keyof Window>(url: string, options: LoadScriptOpti
  * @param options - Script load options with a `map` function to retrieve the return value
  * @returns A promise for the value returned by the `map` function after the script is loaded
  */
-function loadScript<T>(url: string, options: LoadScriptOptions<T>): Promise<T>
-function loadScript<T>(url: string, options?: LoadScriptOptions<T>) {
+function loadScript<T>(url: string, options: LoadScriptOptions & { map: (event: Event) => T }): Promise<T>
+function loadScript(url: string, options?: LoadScriptOptions & { map?: (keyof Window) | ((event: Event) => any) }) {
 	const { cache = false, map } = options ?? {}
 	const promise = LSPromise.get(url, cache, options)
-	return promise.map<T>(map)
+	return promise.map(map)
 }
 
 export default loadScript
