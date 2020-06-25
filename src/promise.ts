2f675d6bd206b78773a4735ea99ca599f6222fea
Sean Wilson <seancwilson@gmail.com>
2020-06-25 10:29:52 -0500

    fix indentation

--- src/promise.ts
+++ src/promise.ts
@@ -48,11 +48,11 @@ export default class LoadScriptPromise implements PromiseLike<Event> {
 	/** The script element created by the promise */
 	element!: HTMLScriptElement
 	/**
-     * Attaches callbacks for the resolution and/or rejection of the Promise.
-     * @param onfulfilled The callback to execute when the Promise is resolved.
-     * @param onrejected The callback to execute when the Promise is rejected.
-     * @returns A Promise for the completion of which ever callback is executed.
-     */
+	 * Attaches callbacks for the resolution and/or rejection of the Promise.
+	 * @param onfulfilled The callback to execute when the Promise is resolved.
+	 * @param onrejected The callback to execute when the Promise is rejected.
+	 * @returns A Promise for the completion of which ever callback is executed.
+	 */
 	then: Promise<Event>['then']
 
 	/**
