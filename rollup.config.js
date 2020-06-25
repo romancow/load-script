75deaf9c9e3541e5fb31fc4f8dab4d2dfbf12fb9
Sean Wilson <seancwilson@gmail.com>
2020-06-25 12:54:16 -0500

    build module & umd w/ rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,28 @@
 import pkg from './package.json'
+import typescript from '@rollup/plugin-typescript'
 import consts from 'rollup-plugin-consts'
 import { terser } from 'rollup-plugin-terser'
 
 export default {
-	input: pkg.module,
+	input: pkg.rollup,
 	context: 'window',
-	output: {
-		name: 'loadScript',
-		file: pkg.browser,
-		format: 'umd',
-		sourcemap: true,
-		sourcemapExcludeSources: true
-	},
+	output: [
+		{
+			file: pkg.module,
+			format: 'module',
+			sourcemap: true,
+			sourcemapExcludeSources: true
+		},
+		{
+			name: 'loadScript',
+			file: pkg.browser,
+			format: 'umd',
+			sourcemap: true,
+			sourcemapExcludeSources: true
+		}
+	],
 	plugins: [
+		typescript(),
 		consts({ version: pkg.version }),
 		terser()
 	]
